1363de224d3196bae1fdd1bfe8b4b56a6634aff9
jimmy_hawkins <mbouzoulasdimitris@gmail.com>
2025-05-16 12:03:35 +0300

    added tests
    stable system

---
+++ UNIT_TESTS/test_single_step_outcome.py
@@ -0,0 +1,29 @@
+import asyncio
+from asyncua import Client, ua
+import asyncua
+import sys
+from colorama import Fore, Back, Style
+
+sys.path.append("..")
+
+from headers import TestSystem
+from headers.server_setup_dev import OPCUAFMUServerSetup
+from headers.config_loader import DataLoaderClass
+
+async def main(funciton):
+    conf = "./test_files/single_step_test.yaml"
+    tests = TestSystem(config_file=conf)
+    if   funciton == "test":     
+        value =  await tests.main_testing_loop()
+        print(f"THIS IS AFTER THE VALUE {value}")
+        exit()
+    elif funciton == "describe": 
+        await tests.describe_system()
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if len(args) == 2 and args[0] == '-func':
+        asyncio.run(main(funciton=args[1]))
+    else:
+        print("args reuqired '-func' and it can be rither 'test' or 'describe' ")
+   
