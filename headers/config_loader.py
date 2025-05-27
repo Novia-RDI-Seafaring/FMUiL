66bda3c06a7940e0fec9b50725da6bb9498ae7dd
jimmy_hawkins <mbouzoulasdimitris@gmail.com>
2025-05-27 10:22:02 +0300

    removed headers

--- headers/config_loader.py
+++
@@ -1,9 +0,0 @@
-import yaml
-
-class DataLoaderClass:
-    def __init__(self, file_path):
-        self.data = self.load_file(file_path= file_path)
-
-    def load_file(self, file_path):
-        with open(file_path) as file:
-            return yaml.safe_load(file)
