c9e50c3a311ee830315077b1b939c3da59d57f65
jimmy_hawkins <mbouzoulasdimitris@gmail.com>
2025-05-27 11:36:18 +0300

    clean up main file

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-import asyncio
-import sys
-# from headers import TestSystem
 from OPCUA_FMU_Simulator import TestSystem
 
-async def main(function):
-    conf = "TESTS/"
-    tests = TestSystem(config_folder= conf)#, remote_servers= remote_servers)
-    if   function == "test":     await tests.main_testing_loop()
+test_directory = "TESTS/"
+
+async def main():
+    tests = TestSystem(config_folder= test_directory)
+    await tests.main_testing_loop()
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    if len(args) == 2 and args[0] == '-func':
-        asyncio.run(main(function=args[1]))
-    else:
-        raise Exception("args required '-func' and it can be rither 'test' or 'describe' ")
+    main()
