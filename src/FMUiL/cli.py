6a357cf983e46fa8128786d428bc178268785e25
UTU\ojklem <kristian.klemets@utu.fi>
2025-10-07 14:03:35 +0300

    feat: --port and --experiment-dir option working in cli. FMU path handling needs rework and fmu path option for cli

--- src/FMUiL/cli.py
+++ src/FMUiL/cli.py
@@ -10,7 +10,8 @@ async def run_experiments(experiment_configs: list[str], port: int = 7500):
     experiments = ExperimentSystem(experiment_configs=experiment_configs, base_port=port)
     await experiments.main_experiment_loop()
 
-# TODO: muokkaa --experiment-dir option selkeemmäksi
+# TODO: muokkaa --experiment-dir option komento selkeemmäksi
+# --experiment-dir toimii, mutta hakee rootista fmu pathia, joten sitä pitää jotenkin muokata
 @app.command()
 def run_all(
     experiments_dir: str = typer.Option("experiments/", help="Directory containing experiment YAML files"),
